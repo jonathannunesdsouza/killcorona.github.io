c5a83b0f296dc8154adf2559d6630ca8f2ae44b6
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2020-12-24 04:34:29 -0300

    Criando posições randômicas de acordo com a resolução disponível do dispositivo.

--- js/jogo.js
+++ js/jogo.js
@@ -1,8 +1,20 @@
-var altura
-var largura 
+var altura = 0
+var largura = 0
 
+//função encarregada de ajustar o tamanho do palco/cenário do jogo de acordo com a resolução da tela do dispositivo, está função está sendo
+//chamada no evento onresize na tag body
 function ajustarTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
     console.log('h: ' + altura + ' | ' + 'w: ' + largura)
 }
+
+ajustarTamanhoPalcoJogo()
+
+
+   var posicaoX = Math.floor(Math.random() * largura)
+   var posicaoY = Math.floor(Math.random() * altura)
+
+   console.log(posicaoX, posicaoY)
+   
+   
