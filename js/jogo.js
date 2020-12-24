df471313315587674b61a971f6fa3df45f4f46de
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2020-12-24 04:11:05 -0300

    Criando função que ajusta o tamanho do palco/cenário do jogo de acordo com a resolução do dispositivo em que a aplicação está sendo executada.

---
+++ js/jogo.js
@@ -0,0 +1,8 @@
+var altura
+var largura 
+
+function ajustarTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+    console.log('h: ' + altura + ' | ' + 'w: ' + largura)
+}
